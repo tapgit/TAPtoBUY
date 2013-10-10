e557436b05217783ca03174752caecbb39867c63
Kidany <kidobv@gmail.com>
2013-10-10 00:18:05 -0400

    mas del gui, product info y sellAn Item

--- TAPtoBUY/src/com/gui/taptobuy/activity/BidProductInfoActivity.java
+++ TAPtoBUY/src/com/gui/taptobuy/activity/BidProductInfoActivity.java
@@ -1,4 +1,5 @@
 package com.gui.taptobuy.activity;
+import com.gui.taptobuy.Entities.ProductForAuction;
 import com.gui.taptobuy.phase1.R;
 
 import android.app.Activity;
@@ -7,16 +8,33 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BidProductInfoActivity extends Activity implements OnClickListener{
 
 	private Button placeBid;
 	private EditText bidInput;
+	public static ProductForAuction BidProduct;	
+	private ImageView prodPic;
+	private TextView prodTitle, prodId, prodTime, prodBrand, prodDimen, prodDescrip, prodSellerUserN, prodPrice, prodShipPrice;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.productinfo_bid);
+		
+		prodPic = (ImageView) findViewById(R.id.BidInfoProdPic);
+		prodTitle = (TextView) findViewById(R.id.BidInfoProdTitle);
+		prodId = (TextView) findViewById(R.id.BidInfoProdID);
+		prodTime = (TextView) findViewById(R.id.BidInfoTimeLeft);
+		prodBrand = (TextView) findViewById(R.id.BidInfoProdBrand);
+		prodDimen = (TextView) findViewById(R.id.BidInfoDimensions);
+		prodDescrip = (TextView) findViewById(R.id.BidInfoDescription);
+		prodSellerUserN = (TextView) findViewById(R.id.BidInfoUserName);
+		prodPrice = (TextView) findViewById(R.id.BidInfoPlaceBidInput);
+		prodShipPrice = (TextView) findViewById(R.id.BidInfoShippingPrice);
+		
 		
 		placeBid = (Button) findViewById(R.id.BidInfoPlaceBidb);
 		bidInput = (EditText) findViewById(R.id.BidInfoPlaceBidInput);
