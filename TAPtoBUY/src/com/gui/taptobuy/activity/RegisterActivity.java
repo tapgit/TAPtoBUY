ef7134cdca403fb9165245d1c7b0c1f17d798c6b
kebinbin <kevin.castillo@upr.edu>
2013-10-09 21:12:09 -0400

    register check for empty values

--- TAPtoBUY/src/com/gui/taptobuy/activity/RegisterActivity.java
+++ TAPtoBUY/src/com/gui/taptobuy/activity/RegisterActivity.java
@@ -51,7 +51,7 @@ public class RegisterActivity extends Activity {
 	private EditText creditCardHoldersNameET;
 	private EditText creditCardExpDateET;
 
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -63,56 +63,118 @@ public class RegisterActivity extends Activity {
 
 	private void initialize() {
 		// TODO Auto-generated method stub
-		
+
 		firstnameET = (EditText) findViewById(R.id.etUserNameF);
 		lastnameET = (EditText) findViewById(R.id.etUserNameL);
 		usernameET = (EditText) findViewById(R.id.etUserName);
 		password1ET = (EditText) findViewById(R.id.etUserPassword1);
 		password2ET = (EditText) findViewById(R.id.etUserPassword2);
 		emailET = (EditText) findViewById(R.id.etUserEmail);
-		
+
 		shipCountryET = (EditText) findViewById(R.id.etShipUserCountry);
-//FALTA:shipContactNameET = (EditText) findViewById(R.id.et);
+		//FALTA:shipContactNameET = (EditText) findViewById(R.id.et);
 		shipStreetET = (EditText) findViewById(R.id.etShipUserStreetAdd);
 		shipCityET = (EditText) findViewById(R.id.etShipUserCity);
 		shipStateET = (EditText) findViewById(R.id.etShipUserState);
 		shipZipCodeET = (EditText) findViewById(R.id.etShipUserZipCode);
 		shipTelephoneET = (EditText) findViewById(R.id.etShipUserTelNum);
-		
+
 		billCountryET = (EditText) findViewById(R.id.etBillUserCountry);
-//FALTA:billContactNameET = (EditText) findViewById(R.id.etUserNameF);
+		//FALTA:billContactNameET = (EditText) findViewById(R.id.etUserNameF);
 		billStreetET = (EditText) findViewById(R.id.etBillUserStreetAdd);
 		billCityET = (EditText) findViewById(R.id.etBillUserCity);
 		billStateET = (EditText) findViewById(R.id.etBillUserState);
 		billZipCodeET = (EditText) findViewById(R.id.etBillUserZipCode);
 		billTelephoneET = (EditText) findViewById(R.id.etBillUserTelNum);
-		
+
 		creditCardNumberET = (EditText) findViewById(R.id.etCreditCardNum);
 		creditCardHoldersNameET = (EditText) findViewById(R.id.etCreditCardHolderName);
 		creditCardExpDateET = (EditText) findViewById(R.id.etCreditCardExpirationDate);
 
 
-		
 
-		
+
+
 		Button submitInf = (Button) findViewById(R.id.bSubmitUserInf);
 		submitInf.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 				if(v.equals((Button) findViewById(R.id.bSubmitUserInf)))
 				{
-					if(!password1ET.getText().toString().equals(password2ET.getText().toString()) ) {			
-						Toast.makeText(RegisterActivity.this, "Passwords don't match, try again", Toast.LENGTH_LONG).show();			
+					if(firstnameET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter your first name.", Toast.LENGTH_SHORT).show();										
 					}
-					//Chequiar si estan vacios
+					else if(lastnameET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter your last name.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(usernameET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter a username.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(password1ET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter a password.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(password2ET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please confirm your password.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(emailET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please anter an email.", Toast.LENGTH_SHORT).show();										
+					}		
+					else if(shipCountryET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the country for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(shipStreetET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the street for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(shipCityET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the city for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(shipStateET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the state for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(shipZipCodeET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the zipcode for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(shipTelephoneET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the telephone for shipping address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billCountryET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the country for billing address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billStreetET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the street for billing address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billCityET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the city for billing address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billStateET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the state for billing address..", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billZipCodeET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the zipcode for billing address.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(billTelephoneET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the telephone for billing address.", Toast.LENGTH_SHORT).show();										
+					}	
+					else if(creditCardNumberET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter your creditcard number.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(creditCardHoldersNameET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the creditcard holder's name.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(creditCardExpDateET.getText().toString().equals("")){
+						Toast.makeText(RegisterActivity.this, "Please enter the creditcard expiration date.", Toast.LENGTH_SHORT).show();										
+					}
+					else if(!password1ET.getText().toString().equals(password2ET.getText().toString()) ) {			
+						Toast.makeText(RegisterActivity.this, "Passwords don't match, try again", Toast.LENGTH_SHORT).show();			
+					}
 					else{
 						new registerTask().execute();
 					}	
 				}
 			}       	
-        });	
+		});	
 	}
 	private int register(){
 		int result = -1;
@@ -130,7 +192,7 @@ public class RegisterActivity extends Activity {
 			json.put("email", emailET.getText().toString());
 			JSONObject shipObj = new JSONObject();
 			shipObj.put("country", shipCountryET.getText().toString());
-//FALTA		shipObj.put("contact_name", "Contact!");
+			//FALTA		shipObj.put("contact_name", "Contact!");
 			shipObj.put("street", shipStreetET.getText().toString());
 			shipObj.put("city", shipCityET.getText().toString());
 			shipObj.put("state", shipStateET.getText().toString());
@@ -139,7 +201,7 @@ public class RegisterActivity extends Activity {
 			json.put("shipping_address",shipObj);
 			JSONObject billObj = new JSONObject();
 			billObj.put("country", billCountryET.getText().toString());
-//FALTA 	billObj.put("contact_name", "Contact!");
+			//FALTA 	billObj.put("contact_name", "Contact!");
 			billObj.put("street", billStreetET.getText().toString());
 			billObj.put("city", billCityET.getText().toString());
 			billObj.put("state", billStateET.getText().toString());
@@ -151,7 +213,7 @@ public class RegisterActivity extends Activity {
 			crCard.put("holders_name", creditCardHoldersNameET.getText().toString());
 			crCard.put("exp_date", creditCardExpDateET.getText().toString());
 			json.put("credit_card", crCard);
-			
+
 			StringEntity entity = new StringEntity(json.toString());
 			post.setEntity(entity);
 
@@ -170,7 +232,7 @@ public class RegisterActivity extends Activity {
 		return result;
 	}
 	public class registerTask extends AsyncTask<Void,Void,Integer> {
-		
+
 		protected Integer doInBackground(Void... params) {
 			return register();
 		}
