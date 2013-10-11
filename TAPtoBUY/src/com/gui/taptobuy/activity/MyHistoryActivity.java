f3fe52737dd0eaaa26b0054e66153e67fccc0cd9
Kidany <kidobv@gmail.com>
2013-10-11 11:32:44 -0400

    mas adapters

--- TAPtoBUY/src/com/gui/taptobuy/activity/MyHistoryActivity.java
+++ TAPtoBUY/src/com/gui/taptobuy/activity/MyHistoryActivity.java
@@ -1,15 +1,47 @@
 package com.gui.taptobuy.activity;
 
+import java.util.ArrayList;
+
+import com.gui.taptobuy.Entities.Product;
+import com.gui.taptobuy.customadapter.BiddingsCustomListAdapter;
+import com.gui.taptobuy.customadapter.MyHistoryBoughtListCustomAdapter;
 import com.gui.taptobuy.phase1.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MyHistoryActivity extends Activity{
+	public  ArrayList<Product> historyBoughtItems;
+	public  ArrayList<Product> historySoldItems;
+	private ListView itemsList;
+	private LayoutInflater layoutInflator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.account_myhistory);
+		this.layoutInflator = LayoutInflater.from(this);
+		this.itemsList = (ListView) findViewById(R.id.myHistory_boughtItems);
+		itemsList.invalidateViews();		
+		itemsList.setAdapter(new MyHistoryBoughtListCustomAdapter(this,this.layoutInflator, historyBoughtItems));
+		this.itemsList = (ListView) findViewById(R.id.myHistory_SoldItems);
+		itemsList.invalidateViews();		
+		itemsList.setAdapter(new MyHistoryBoughtListCustomAdapter(this,this.layoutInflator, historySoldItems));
+		
 	}
+	
+	public static class MyViewHistory {
+		public TextView productName, sellerUserName, priceAndShiping,bidsAmount,timeRemaining;
+		public RatingBar sellerRating;		
+		public ImageView itemPic;
+		public Product item;		
+
+		}
 }
--- TAPtoBUY/src/com/gui/taptobuy/customadapter/MyHistoryBoughtListCustomAdapter.java
+++ TAPtoBUY/src/com/gui/taptobuy/customadapter/MyHistoryBoughtListCustomAdapter.java
@@ -1,12 +1,33 @@
 package com.gui.taptobuy.customadapter;
 
+import java.util.ArrayList;
+
+import com.gui.taptobuy.Entities.Product;
+import com.gui.taptobuy.activity.MyHistoryActivity;
+import com.gui.taptobuy.activity.SearchActivity;
+import com.gui.taptobuy.activity.MyBiddingActivity.MyViewAuctionItem;
+import com.gui.taptobuy.phase1.R;
+
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-public class MyHistoryBoughtListCustomAdapter extends BaseAdapter implements OnClickListener {
 
+public class MyHistoryBoughtListCustomAdapter extends BaseAdapter implements OnClickListener {
+	private MyHistoryActivity activity;
+	private LayoutInflater layoutInflater;
+	private ArrayList<Product> items;	
+
+	public MyHistoryBoughtListCustomAdapter (MyHistoryActivity a, LayoutInflater l, ArrayList<Product> items)
+	{
+		this.activity = a;		
+		this.layoutInflater = l;
+		this.items = items;
+	}
+	
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -26,9 +47,11 @@ public class MyHistoryBoughtListCustomAdapter extends BaseAdapter implements OnC
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(int position, View itemRow, ViewGroup parent) {
+		// TODO Auto-generated method stub	MyViewAuctionItem itemHolder;
+		Product item = items.get(position);
+		
+		itemRow = layoutInflater.inflate(R.layout.bidproduct_row, parent, false); return null;
 	}
 
 	@Override
